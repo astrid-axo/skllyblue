8570b3be061001e75a4db93a80f99694290955a9
skullbite <skullbite@2603-9001-6c00-d6b5-0000-0000-0000-1f1f.inf6.spectrum.com>
2025-07-08 12:31:43 -0400

    init

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,20 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+dnf5 install -y @base-x \
+    @development-tools \
+    gnome-shell \
+    codium \
+    godot \
+    tailscale \
+    distrobox 
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub app.zen_browser.zen
+flatpak install -y flathub com.usebottles.bottles
 
 # Use a COPR Example:
 #
