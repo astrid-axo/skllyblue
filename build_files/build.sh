288556b24bfc23323525589363a3b0102a7fb507
astrid-axo <git2@bork.quest>
2025-07-08 23:51:41 -0400

    switch to bluefin base

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,10 +13,7 @@ set -ouex pipefail
 dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-dnf5 install -y @base-x \
-    @development-tools \
-    gnome-shell \
-    codium \
+dnf5 install -y codium \
     godot \
     tailscale \
     distrobox 
