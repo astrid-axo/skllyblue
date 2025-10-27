336ecc40edbea31ca73145c21ee089b3a5712d8d
astrid-axo <git2@bork.quest>
2025-10-27 10:02:29 -0400

    chunked up steps

--- Containerfile
+++ Containerfile
@@ -2,6 +2,7 @@
 FROM scratch AS ctx
 COPY build_files /
 COPY system_files /system_files
+COPY files /bluebuild_files
 
 # Base Image
 FROM quay.io/fedora/fedora-silverblue:latest
@@ -20,10 +21,41 @@ FROM quay.io/fedora/fedora-silverblue:latest
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 RUN \
-  --mount=type=bind,from=ghcr.io/blue-build/modules:latest,src=/modules,dst=/tmp/modules,rw \
-  --mount=type=bind,from=ghcr.io/blue-build/cli/build-scripts:latest,src=/scripts/,dst=/tmp/scripts/ \
-  /tmp/scripts/run_module.sh 'gnome-extensions' \
-  '{"type":"gnome-extensions","install":["ArcMenu","Just Perfection","Tiling Shell","Panel Workspace Scroll","Tailscale QS","Blur my Shell","Syncthing Indicator","Rounded Window Corners Reborn","Caffeine","Fullscreen Notifications","Hide the dock in overview"]}'
+    --mount=type=bind,from=ghcr.io/blue-build/modules:latest,src=/modules,dst=/tmp/modules,rw \
+    --mount=type=bind,from=ghcr.io/blue-build/cli/build-scripts:latest,src=/scripts/,dst=/tmp/scripts/ \
+    --mount=type=bind,from=ctx,source=/bluebuild_files,target=/tmp/files \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    /tmp/scripts/run_module.sh 'gnome-extensions' \
+    "$(cat /ctx/bluebuild/gnome-extensions.json)" && \
+    # /tmp/scripts/run_module.sh 'default-flatpaks' \
+    # "$(cat /ctx/bluebuild/default-flatpaks.json)" && \
+    CONFIG_DIRECTORY="/tmp/files" tmp/scripts/run_module.sh 'gschema-overrides' \
+    '{"type":"gschema-overrides"}'
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/steps/00-init
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/steps/01-dnf
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/steps/02-themes
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/steps/03-steam-additions
+
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,37 +2,10 @@
 
 set -ouex pipefail
 
-uneeded_apps=("syncthing-start")
+uneeded_apps=("syncthing-start syncthing-ui yelp")
 
-cd /tmp
-git clone https://github.com/shahnawazshahin/steam-using-gamescope-guide
-cd steam-using-gamescope-guide/usr
 
-for f in $(ls ./bin); do
-    cp -r ./bin/$f /usr/bin/$f
-done
 
-cp ./share/wayland-sessions/steam.desktop /usr/share/wayland-sessions/steam.desktop
-cd /tmp
-rm -rf ./steam-using-gamescope-guide
-
-rsync -rvK /ctx/system_files/shared/ /
-ln -s /run /var/run
-
-rm /usr/share/pixmaps/fedora-gdm-logo.png
-cp /usr/share/plymouth/themes/spinner/watermark.png /usr/share/pixmaps/fedora-gdm-logo.png
-
-python3 /ctx/update_os_release.py
-echo "skllyblue" >> /etc/hostname
-
-for f in "skllyjust"; do
-    chmod +x /usr/bin/$f
-done
-
-for f in $(ls /usr/share/icons/skllyblue); do
-    icon=${f%.*}
-    xdg-icon-resource install --size 256 /usr/share/icons/skllyblue/$f skllyblue-$icon
-done
 # shell_ver=$(gnome-shell --version)
 # shell_ver=($shell_ver)
 # shell_ver=${shell_ver[2]}
@@ -59,61 +32,21 @@ done
 #     fi
 # done
 
-dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
-
-dnf5 install -y @development-tools \
-    openssl \
-    fzf \
-    codium \
-    steam \
-    godot \
-    gamescope \
-    tailscale \
-    distrobox \
-    syncthing \
-    just \
-    fastfetch \
-    vim \
-    nautilus-python 
-    
 for f in $uneeded_apps; do
     rm -f /usr/share/applications/$f.desktop
 done
 
-dnf5 remove -y firefox \
-    toolbox \
-    blender \
-    gnome-shell-extension-background-logo \
-    gnome-shell-extension-window-list \
-    gnome-shell-extension-apps-menu \
-    gnome-shell-extension-launch-new-instance \
-    gnome-shell-extension-places-menu \
-    gnome-extensions-app \
-    gnome-help \
-    gnome-tour \
-    gnome-software
 
-gnome_extensions=$(ls /usr/share/gnome-shell/extensions)
 
-# python script packed into a line because uhh
-echo -e "[org.gnome.shell]\nenabled-extensions=$(echo "_=\"\"\"$gnome_extensions\"\"\";print('['+', '.join([f\"\"\"'{i}'\"\"\" for i in _.split('\n')])+']')" | python3)" \
-    >> /usr/share/glib-2.0/schemas/40_skllyblue.gschema.override
-
-glib-compile-schemas /usr/share/glib-2.0/schemas
+#gnome_extensions=$(ls /usr/share/gnome-shell/extensions)
+#python script packed into a line because uhh
+#echo -e "[org.gnome.shell]\nenabled-extensions=$(echo "_=\"\"\"$gnome_extensions\"\"\";print('['+', '.join([f\"\"\"'{i}'\"\"\" for i in _.split('\n')])+']')" | python3)" \
+#    >> /usr/share/glib-2.0/schemas/40_skllyblue.gschema.override
+#cat /usr/share/glib-2.0/schemas/40_skllyblue.gschema.override
+#glib-compile-schemas /usr/share/glib-2.0/schemas
 dconf update
 
-dnf5 -y copr enable lilay/topgrade
-dnf5 -y install topgrade
-dnf5 -y copr disable lilay/topgrade
 
-dnf5 -y copr enable vdanielmo/git-credential-manager 
-dnf5 -y install git-credential-manager
-dnf5 -y copr disable vdanielmo/git-credential-manager
-
-dnf5 -y copr enable umutd3401/extension-manager
-dnf5 -y install extension-manager
-dnf5 -y copr disable umutd3401/extension-manager 
 
 #### Example for enabling a System Unit File
 
